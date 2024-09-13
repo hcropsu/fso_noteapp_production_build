333de788608794ed937a7097fa635cdffd3d06e1
hcropsu <roopesanterisalminen@gmail.com>
2024-09-13 11:44:17 +0300

    backend update

--- index.js
+++ index.js
@@ -51,14 +51,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('api/notes/:id', (request, response) => {
@@ -66,30 +61,26 @@ app.delete('api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id)
   response.status(204).end()
 })
-const generateId = () => {
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(n => Number(n.id)))
-  : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
-    id: generateId()
-  }
-  notes = notes.concat(note)
-  response.json(note)
+    important: body.important || false
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
+
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
