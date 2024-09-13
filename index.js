5ea363eda6a79dbdeb880c71b5860353d46bf612
hcropsu <roopesanterisalminen@gmail.com>
2024-09-13 10:42:07 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Note = require('./models/note')
+
 const requestLogger = (request, response, next) => {
   console.log('Method: ', request.method)
   console.log('Path: ', request.path)
@@ -8,6 +12,7 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -40,7 +45,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
