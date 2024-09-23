93877ff3542f8973e874c43122bb310c2f2951b4
hcropsu <roopesanterisalminen@gmail.com>
2024-09-23 08:26:09 +0300

    osa4d noteApp DONE(token based authorization)

--- controllers/login.js
+++ controllers/login.js
@@ -19,7 +19,24 @@ loginRouter.post('/', async (request, response) => {
     id: user._id
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  // Set the 'expiresIn' property to 60*60 seconds (1 h):
+  // + added security
+  // - less smooth UX
+
+  // 'Server side session' could also be used: every token granted could be
+  // saved to a DB, and for incoming reqs the validity of the token could be
+  // checked.
+  // + revoking a token could be done "instantly"
+  // - added complexity to backend
+  // - querying a DB for the information on token validity is slower than
+  //   checking from the token itself (e.g. 'jwt.verify()' in 'notes.js')
+
+  // When using server side sessions, the token is usually just a "random" string
+  // not containing any information about the user (e.g. user id etc.). The user
+  // info matching the token could be saved to a DB (a fast and specific such as Redis).
+  // In server side sessions, the info on user id is also often transferred using
+  // cookies instead of the 'Authorization' header.
+  const token = jwt.sign(userForToken, process.env.SECRET, {  expiresIn: 60*60 })
 
   response
     .status(200)
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,12 +1,15 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+// GET 'api/notes'
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
+// GET 'api/notes/:id'
 notesRouter.get('/:id', async (request, response) => {
   const note = await Note.findById(request.params.id)
   if (note) {
@@ -16,15 +19,34 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
+// DELETE 'api/notes/:id'
 notesRouter.delete('/:id', async (request, response) => {
   await Note.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
 
+// Helper func to extract token from request header 'authorization'
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+// POST '/api/notes'. First check if req contains valid token,
+// if yes, then proceed.
+// NOTE: "If the app has multiple apis that require a login,
+// JWT validation should be refactored into its own module, or
+// use an existing library such as 'express-jwt'"
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
+  }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
@@ -38,6 +60,7 @@ notesRouter.post('/', async (request, response) => {
   response.status(201).json(savedNote)
 })
 
+// PUT '/api/notes'
 notesRouter.put('/:id', async (request, response) => {
   const { content, important } = request.body
   const updatedNote = await Note.findByIdAndUpdate(request.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
